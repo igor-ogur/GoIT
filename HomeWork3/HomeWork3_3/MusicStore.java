2a9e3883286ac564f1ebeffb5ecd4bba4ed839e7
igor-ogur <igor-ogur@mail.ru>
2016-03-07 13:55:56 +0200

    Update MusicStore.java

--- HomeWork3/HomeWork3_3/MusicStore.java
+++ HomeWork3/HomeWork3_3/MusicStore.java
@@ -4,7 +4,5 @@ package HomeWork3.HomeWork3_3;
  * Created by SKLAD-PC on 04.03.16.
  */
 public class MusicStore {
-    MusicalInstrument guitar = new MusicalInstrument();
-    MusicalInstrument piano = new MusicalInstrument();
-    MusicalInstrument trumpet = new MusicalInstrument();
+    MusicalInstrument musicalInstrument = new MusicalInstrument();
 }
