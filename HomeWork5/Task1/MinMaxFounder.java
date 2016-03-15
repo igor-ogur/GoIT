15867fda05ea4e83b3601c72f832eaf657071d66
igor-ogur <igor-ogur@mail.ru>
2016-03-16 01:09:28 +0200

    added HomeWork5

---
+++ HomeWork5/Task1/MinMaxFounder.java
@@ -0,0 +1,35 @@
+package HomeWork5.Task1;
+
+public class MinMaxFounder {
+
+    public static int getMinElement(int[] array){
+        int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < result) {
+                result = array[i];
+            }
+        }
+        return result;
+    }
+
+    public static int getMaxElement(int[] array){
+        int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > result) {
+                result = array[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = (int) (Math.random()*100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Minimal element of array is " + getMinElement(array));
+        System.out.println("Maximal element of array is " + getMaxElement(array));
+    }
+}
