13eb6474429548cebb38bbd34717733c0ca41d83
Igor Ogerchuk <igor-ogur@mail.ru>
2016-03-16 11:07:44 +0200

    added Task2 to HomeWork5

---
+++ HomeWork5/Task2/ArraysSorter.java
@@ -0,0 +1,36 @@
+package HomeWork5.Task2;
+
+
+public class ArraysSorter {
+
+    public static double[] sortArrayFromMinToMax (double[] array){
+        boolean check = true;
+        while (check) {
+            check = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    double temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    check = true;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        double[] array = new double[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = Math.floor(Math.random()*10000)/100;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        array = sortArrayFromMinToMax(array);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
