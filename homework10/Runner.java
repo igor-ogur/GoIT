6b672c22b6462504003346bdb1d3a1e945bf6ed5
Igor Ogerchuk <igor-ogur@mail.ru>
2016-04-01 16:02:37 +0300

    added homework10

---
+++ homework10/Runner.java
@@ -0,0 +1,9 @@
+package homework10;
+
+public class Runner {
+    public static void main(String[] args) {
+        InputOutputMessenger messenger = new InputOutputMessenger();
+        messenger.readMessageFromFileEncryptAndWriteToAnotherFile("original.txt", "encryptedFile.txt");
+        messenger.readMessageFromFileDecryptAndWriteToAnotherFile("encryptedFile.txt", "decryptedFile.txt");
+    }
+}
